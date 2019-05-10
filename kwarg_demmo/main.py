c9f5c8a49c40b4018b848c84c7734e6ab3ffbae8
MarkWang <markw>
2019-05-10 10:07:50 +0800

    add the new demos

---
+++ kwarg_demmo/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+def parrot(voltage, state='a stiff',action='voom',type='Norwegian Blue'):
+    print("--this parrot wouldn't",action)
+    print("if you put",voltage, "volts through it.")
+    print("Lovely plumage, the",type)
+    print("It's",state,"!")
+
+print('================================================')
+parrot(1000)
+print('================================================')
+parrot(voltage=1000)
+print('================================================')
+parrot('a thousand', state='pushing up the daisies')
+print('================================================')
+
